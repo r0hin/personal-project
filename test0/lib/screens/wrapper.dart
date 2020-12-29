8d46979c67dee15324faefd67d6f62ee0585a1ee
Rohin <me@r0h.in>
2020-12-28 20:19:33 -0500

    :book: Project Structure for Test0 and Authentication
    - No versioning. Will start final project soon sometime.

---
+++ test0/lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:test0/screens/auth/authentication.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
