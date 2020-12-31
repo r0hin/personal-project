118d6d9a1be17dea0d5e806ba64eb71a93b1a6ad
r0hin <me@r0h.in>
2020-12-31 06:50:10 -0500

    Flutter's pretty fun actually hehe

--- test0/lib/screens/wrapper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:test0/screens/auth/authentication.dart';
-
-class Wrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Authenticate();
-  }
-}
